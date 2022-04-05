307d797fa5d5154f65be59811b4f0d65dd9f58d2
Nailtechie817 <britmt817@gmail.com>
2022-04-05 14:14:37 -0400

    initialize react project

---
+++ i-heart-react/src/App.js
@@ -0,0 +1,17 @@
+import HeartsList from './components/HeartsList';
+import Heart from './components/Heart';
+import Header from './components/Header';
+
+
+function App() {
+    return (
+        <>
+            <Header />
+            <HeartsList />
+            <Heart />
+            
+        </>
+    );
+}
+
+export default App;
---
+++ i-heart-react/src/components/Heart.js
@@ -0,0 +1,12 @@
+import HeartLogo from './HeartSVG'
+
+const Heart = (props) => {
+    return (
+        <div className='heart'>
+            <img className='heart-img' src={HeartLogo} alt='heart' />
+            <p className='heart-message'>{props.msg}</p>
+      </div>
+    );
+}
+
+export default Heart;
